8a289d0a20ff8748e3452a508f7e4b5801817b3d
Mobidikt <ghost3299@yandex.ru>
2020-12-11 22:36:32 +0400

    feat: add res

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -204,8 +204,18 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let res = '';
+  if (isStartIncluded === true) {
+    res += '[';
+  } else res += '(';
+  if (a > b) {
+    res += `${b}, ${a}`;
+  } else res += `${a}, ${b}`;
+  if (isEndIncluded === true) {
+    res += ']';
+  } else res += ')';
+  return res;
 }
 
 
@@ -221,8 +231,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -238,10 +248,11 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  const str = num.toString();
+  const res = str.split('').reverse().join('');
+  return +res;
 }
-
 
 /**
  * Validates the CCN (credit card number) and return true if CCN is valid
@@ -281,8 +292,16 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let res = 0;
+  const str = num.toString();
+  for (let i = 0; i < str.length; i += 1) {
+    res += +str[i];
+  }
+  if (res < 10) {
+    return res;
+  }
+  return getDigitalRoot(res);
 }
 
 
